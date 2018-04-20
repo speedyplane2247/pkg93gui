21c961b68464c270716a31c1a1d33559617c46f4
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2018-04-20 18:56:39 -0400

    Beta 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-// coming soonTM
+function checkRepo(repo) {
+    document.getElementById("consoleoutput") = $exe("pkg93 add-repo "+repo) + pkg93.pull()
+    getInstalled();
+}
+function deleteRepo(repoX) {
+    document.getElementById("consoleoutput") = $exe("pkg93 rm-repo "+repoX) + pkg93.pull()
+}
+function install(toInstall) {
+    document.getElementById("consoleoutput") = pkg93.get(toInstall)
+}
+function listPackage() {
+    pkg93.pull()
+    document.getElementById("packagelist") = pkg93.getConfig().pkglist
+}
+function getInstalled() {
+    document.getElementById("installedpkg") = pkg93.getConfig().installed
+}
+function Uninstall(toUninstall) {
+    document.getElementById("consoleoutput") = pkg93.rm(toUninstall)
+}
